ebb6547c667639a5e2f0af59937cb420ac3e1be5
bchang806 <bchang806@gmail.com>
2019-05-13 18:07:03 -0400

    updated

--- employeeRecordSystem/src/com/boChungChang/Main.java
+++ employeeRecordSystem/src/com/boChungChang/Main.java
@@ -5,11 +5,12 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-    	List<Employee> employee = new ArrayList<Employee>();  	
-    	loadDefaultEmployee(employee);  	
-    	Menu menu = new Menu();
-    	menu.startMenu(employee);
+    	List<Employee> employee = new ArrayList<Employee>(); // A List is used to store each employee as an object
+    	loadDefaultEmployee(employee);  	// Load Current Employees in the Company
+    	Menu menu = new Menu(); // Create a menu object
+    	menu.startMenu(employee); //Generate a menu for users to select options
     } 
+    
     
     public static void loadDefaultEmployee(List<Employee> employee) {
     	Employee john = new Manager("John", "Doe", "john@gamil.com", 100000);
